810530463bd81fde386e0b1cb522c34f4d21ae4e
Kevin <xyu.kevin@gmail.com>
2021-01-16 18:31:06 +0800

    Fix offline mode local doc path check

--- extension/settings.js
+++ extension/settings.js
@@ -1,7 +1,8 @@
 // Don't use /g mode, otherwise regex.test() would return an alternating result.
 // See https://stackoverflow.com/a/2630538/2220110
-const REGEX_DOC_PATH_FILE = /(^file:\/\/.*\/reference\/en\/)(.*)/i;
-const REGEX_DOC_PATH_HTTP = /(^https?:\/\/.*:\d{2,6}\/en\/)(.*)/i;
+const REGEX_DOC_PATH_FILE = /(file:\/\/)?(.*)/i;
+const REGEX_DOC_PATH_HTTP = /(^https?:\/\/.*)(:\d{2,6})?(.*)/i;
+const FILE_PROTO = 'file://';
 
 const settings = {
     get language() {
@@ -20,10 +21,11 @@ const settings = {
         return localStorage.getItem('offline-path');
     },
     set offlineDocPath(path) {
+        if (path.startsWith('/')) {
+            path = FILE_PROTO + path;
+        }
         for (let regex of [REGEX_DOC_PATH_FILE, REGEX_DOC_PATH_HTTP]) {
             if (regex.test(path)) {
-                // Use regex match rule to eliminate the tail path
-                path = path.replace(regex, "$1");
                 localStorage.setItem('offline-path', path);
                 return;
             }
@@ -31,6 +33,14 @@ const settings = {
     },
     // Use regex patterns to check user local doc path validity.
     checkDocPathValidity(path) {
+        if (path.startsWith('/')) {
+            path = FILE_PROTO + path;
+            console.log("app file proto: ", path)
+        }
+        if(!path.startsWith('file://') && !path.startsWith("http")){
+            return false;
+        }
+
         return REGEX_DOC_PATH_FILE.test(path) || REGEX_DOC_PATH_HTTP.test(path);
     }
 };
